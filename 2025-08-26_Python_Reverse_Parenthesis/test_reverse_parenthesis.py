ec1702b63309443e30163095676e8b816464c563
Nils Eckmann <nils.eckmann@mein.gmx>
2026-02-17 16:57:44 +0100

    day 16 - python - comments added in test file

--- 2025-08-26_Python_Reverse_Parenthesis/test_reverse_parenthesis.py
+++ 2025-08-26_Python_Reverse_Parenthesis/test_reverse_parenthesis.py
@@ -2,18 +2,20 @@ import main_reverse_parenthesis as mrp
 
 
 if __name__ == "__main__":
+    # Test 1
     if mrp.decode("(f(b(dc)e)a)") == "abcdef":
         print("Test1: PASSED")
     else:
         print("Test1: FAILED")
 
+    # Test 2
     if mrp.decode("((is?)(a(t d)h)e(n y( uo)r)aC)") == "Can you read this?":
         print("Test1: PASSED")
     else:
         print("Test1: FAILED")
 
+    # Test 3
     if mrp.decode("f(Ce(re))o((e(aC)m)d)p") == "freeCodeCamp":
         print("Test1: PASSED")
     else:
         print("Test1: FAILED")
-
